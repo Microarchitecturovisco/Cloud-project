3617dcfe1a6f1b1a13f7cb99e88db48b9f597653
Krzysztof Nazar <s184698@student.pg.edu.pl>
2024-05-29 15:40:09 +0200

    set up two microservices

---
+++ user-service/src/test/java/cloud/project/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.project.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
