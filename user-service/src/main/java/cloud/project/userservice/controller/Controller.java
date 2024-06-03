aa282210273074ebc6547a4fdec77f57b84aafcd
CzajaLukasz <czajalukasz5@gmail.com>
2024-06-03 23:02:55 +0200

    implemented base to api - not working yet

--- user-service/src/main/java/cloud/project/userservice/controller/Controller.java
+++ user-service/src/main/java/cloud/project/userservice/controller/Controller.java
@@ -4,12 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/user")
 public class Controller {
 
     private final Environment environment;
